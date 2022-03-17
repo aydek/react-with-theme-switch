90b53d04ef63314a526998e9eb489484e2705a67
aydek <56939339+aydek@users.noreply.github.com>
2022-03-17 00:24:45 +0000

    Update App.tsx
    Added global style

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { createGlobalStyle } from 'styled-components';
 import { Header } from './components/Header';
 import './styles/root.scss';
 
@@ -20,8 +21,16 @@ const App = () => {
         localStorage.colorMode = theme;
     }, [theme]);
 
+    const GlobalStyle = createGlobalStyle`
+        body {
+            color: ${theme === 'light' ? '#222' : '#whitesmoke'};
+            background-color: ${theme === 'light' ? 'whitesmoke' : '#222'};
+        }
+    `;
+
     return (
         <ThemeContext.Provider value={{ theme, setTheme }}>
+            <GlobalStyle />
             <div>
                 <Header></Header>
             </div>
